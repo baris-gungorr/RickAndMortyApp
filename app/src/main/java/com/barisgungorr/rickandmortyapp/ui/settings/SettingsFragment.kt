9670b587c5d88dfea1b239392c7b3be709c04dfe
Baris Gungor <gungorbaris41@gmail.com>
2024-01-14 13:33:12 +0300

    Created popopmenu for language selection feature on settings page Added settingviewmodel in accordance with mvvm architecture

--- app/src/main/java/com/barisgungorr/rickandmortyapp/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/barisgungorr/rickandmortyapp/ui/settings/SettingsFragment.kt
@@ -1,17 +1,22 @@
 package com.barisgungorr.rickandmortyapp.ui.settings
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.fragment.findNavController
 import com.barisgungorr.rickandmortyapp.R
 import com.barisgungorr.rickandmortyapp.databinding.FragmentFavoriteBinding
 import com.barisgungorr.rickandmortyapp.databinding.FragmentSettingsBinding
+import com.barisgungorr.rickandmortyapp.ui.activity.MainActivity
+import java.util.Locale
 
 
 class SettingsFragment : Fragment() {
@@ -27,6 +32,7 @@ class SettingsFragment : Fragment() {
         binding = FragmentSettingsBinding.inflate(inflater, container, false)
         transition()
         themeMode()
+        selectLanguage()
         return binding.root
     }
 
@@ -34,7 +40,6 @@ class SettingsFragment : Fragment() {
         binding.ivHome.setOnClickListener {
             findNavController().navigate(R.id.actionSettingsToMain)
         }
-
     }
 
     private fun themeMode() {
@@ -61,6 +66,43 @@ class SettingsFragment : Fragment() {
         }
     }
 
+    private fun selectLanguage() = with(binding) {
+        btnLanguage.setOnClickListener{
+            val popup = PopupMenu(requireContext(), btnLanguage)
+            popup.menuInflater.inflate(R.menu.popup_menu, popup.menu)
+            popup.show()
+
+            popup.setOnMenuItemClickListener { item ->
+                when(item.itemId) {
+                    R.id.action_turkish -> {
+                        val language = "tr"
+                        val locale = Locale(language)
+                        Locale.setDefault(locale)
+                        val config = Configuration()
+                        config.locale = locale
+                        requireContext().resources.updateConfiguration(config, requireContext().resources.displayMetrics)
+                        val intent = Intent(requireContext(), MainActivity::class.java)
+                        startActivity(intent)
+                        requireActivity().finish()
+                        true
+                    }
+                    R.id.action_english -> {
+                        val language = "en"
+                        val locale = Locale(language)
+                        Locale.setDefault(locale)
+                        val config = Configuration()
+                        config.locale = locale
+                        requireContext().resources.updateConfiguration(config, requireContext().resources.displayMetrics)
 
 
+                        val intent = Intent(requireContext(), MainActivity::class.java)
+                        startActivity(intent)
+                        requireActivity().finish()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/barisgungorr/rickandmortyapp/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,13 @@
+package com.barisgungorr.rickandmortyapp.ui.settings
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingsViewModel@Inject constructor(
+
+): ViewModel(){
+
+}
